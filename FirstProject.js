0601d50e9a2215a498dc5453386daddecaa49507
Swagsticka <ninjaman306@gmail.com>
2015-03-25 18:17:28 -0500

    A fancy Game!

---
+++ FirstProject.js
@@ -0,0 +1,33 @@
+var i = 10
+
+var spam = 1
+
+var slaying = true
+
+var youHit = Math.floor(Math.random() * 3)
+
+var damageThisRound = Math.floor(Math.random() * 6 + 2)
+
+var totalDamage = 0
+// The story \/
+alert("You wake up in a strange land... ");
+alert("You have a sudden uncontrollable \
+desire to kill a dragon... ");
+var prompt_1 = confirm("Will you accept the Challenge?")
+if (prompt_1 === true) {
+    while(slaying) {
+      if (youHit) {
+          alert("You scraped the dragon");
+          totalDamage += damageThisRound;
+               if (totalDamage >= 4) {
+                  alert("You joined killed the dragon!");
+                 slaying = false
+              } else {
+                   youHit = Math.floor(Math.random() * 2)
+             }
+      } else {
+          alert("The dragon killed you");
+          slaying = false
+      }
+    }
+} 
